323872eb9c0ead2af9e3e87b0df73345ed5870de
Alexander Roddis <alexander.roddis@giffits.de>
2019-05-20 17:15:05 +0200

    sdfsdfsdf

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,4 +1,10 @@
 const express = require('express');
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 function setupViewEngine(app){
     var exphbs  = require('express-handlebars');
@@ -7,6 +13,28 @@ function setupViewEngine(app){
     app.set('view engine', 'handlebars');
 }
 
+function setupSecurity(app){
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        done(null, {username: "admin", id: 1});
+    });
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.use(new LocalStrategy(
+        function(username, password, done) {
+            if(username === "admin" && password === "test"){
+                return done(null, {username: "admin", id: 1});
+            }
+
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+    ));
+}
 async function setupDatabase(){
     return new Promise(function(resolve, reject){
         var mongoose = require('mongoose');
@@ -22,7 +50,19 @@ module.exports.start = function(){
   return new Promise(function(resolve, reject){
       let app = express();
 
+      app.use(cookieParser());
+      app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+      app.use(session({
+          secret: 'dfjkgndiungdifngiednfgidunguidfguidnugdifg',
+          resave: false,
+          saveUninitialized: true,
+          cookie: { secure: true }
+      }));
+      app.use(flash());
       setupViewEngine(app);
+
+      setupSecurity(app);
 
       setupDatabase().then(() => {
           return resolve(app);
--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,20 @@
 const DestinationController = require('./../src/Controller/DestinationController');
+let passport = require('passport');
 
 module.exports.setupRoutes = function(app){
-    app.get('/', function (req, res) {
+    app.get('/',passport.authenticate('local'), function (req, res) {
         res.render("home", {
             name: "Was du willst"
         });
     });
 
+    app.post('/login', (req, res) => passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', })(req, res));
+
+    app.get('/login', function (req, res) {
+        res.render("login");
+    });
+
     app.get('/api/destination', DestinationController.apiGetDestinations);
 
     app.get('/api/destination/:id', DestinationController.apiGetDestination);
-
 };
